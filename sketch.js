9ae603247c8502ca32d2eeeb4aff9cb378afcc8b
Pete <peter@Peters-MBP.modem>
2021-11-02 13:32:06 +1100

    cursor smudge

--- sketch.js
+++ sketch.js
@@ -1,49 +1,41 @@
-// in this sketch we're going to send the webcam to the shader, and then invert it's colors
 
-// the shader variable
-let camShader;
-
-let pupImg;
+let archiveImg;
 let bufferImg;
+let dispMap;
 
 function preload(){
-  // load the shader
-  camShader = loadShader('effect.vert', 'effect.frag');
-  pupImg = loadImage('textTest.png');
-
+  archiveImg = loadImage('mortonObituary.jpg');
 }
 
 function setup() {
-  // shaders require WEBGL mode to work
-  //createCanvas(windowWidth, windowHeight, WEBGL);
   createCanvas(windowWidth, windowHeight);
   noStroke();
   pixelDensity(1);
-  bufferImg = createImage(pupImg.width,pupImg.height)
-
+  dispMap = createGraphics(600, 600);
+  bufferImg = createImage(archiveImg.width,archiveImg.height);
+  dispMap.fill(0);
+  dispMap.textSize(790);
+  dispMap.text('G',10,580);
+  //dispMap.ellipse(dispMap.width/2,dispMap.height/2,dispMap.width)
 }
 
 function draw() {
-  // shader() sets the active shader with our shader
-  // shader(camShader);
-  // camShader.setUniform('tex1', pupImg);
-  // camShader.setUniform('x', map(mouseX,width,0,0,1));
-  // camShader.setUniform('y', map(mouseY,0,height,0,1));
-  // // rect gives us some geometry on the screen
-  // rect(0,0,10, 10);
 
-
-  bufferImg.copy(pupImg,0,0,pupImg.width,pupImg.height,0,0,bufferImg.width,bufferImg.height);
-  pupImg.loadPixels();
+  bufferImg.copy(archiveImg,0,0,archiveImg.width,archiveImg.height,0,0,bufferImg.width,bufferImg.height);
+  archiveImg.loadPixels();
   bufferImg.loadPixels();
+  dispMap.loadPixels();
 
      var mX = int(mouseX);
      var mY = int(mouseY);
-     var smudgeSize = 180;
+     var smudgeSize = 300;
+     var xCount = 0;
+     var yCount = 0;
 
       for(var y = mY-smudgeSize; y < mY+smudgeSize; y++){
         for(var x = mX-smudgeSize; x <mX+smudgeSize; x++){
-          var index = (x+y*pupImg.width)*4;
+          var index = (x+y*archiveImg.width)*4;
+          var mapIndex = (xCount+yCount*dispMap.width)*4;
           var displacement;
 
           //up positive
@@ -53,7 +45,7 @@ function draw() {
           //1 on and 0 off
           var vertBool = 1;
           var horizBool = 1;
-
+          //check horizontal mouse direction
           if(mouseX<pmouseX){
             horizDir = 1;
             //horizBool = 1;
@@ -64,7 +56,7 @@ function draw() {
             horizBool = 0;
             horizDir = 1;
           }
-
+          //check vertical mouse direction
           if(mouseY < pmouseY){
             vertDir = 1;
           } else if(mouseY > pmouseY){
@@ -74,21 +66,30 @@ function draw() {
             vertDir = 1;
           }
 
-
-          //this algorithm coŒvers all settings
-          displacement = index+((pupImg.width*4)*vertDir*vertBool)+horizBool*4*horizDir;
-
-          pupImg.pixels[index]   = bufferImg.pixels[displacement];
-          pupImg.pixels[index+1] = bufferImg.pixels[displacement+1];
-          pupImg.pixels[index+2] = bufferImg.pixels[displacement+2];
-          pupImg.pixels[index+3] = 255;
-
+          //this takes the black pixels as the map
+          if(dispMap.pixels[mapIndex+3] != 0){
+            //this algorithm covers all settings
+            displacement = index+((archiveImg.width*4)*vertDir*vertBool)+horizBool*4*horizDir;
+            //replace
+            archiveImg.pixels[index]   = bufferImg.pixels[displacement];
+            archiveImg.pixels[index+1] = bufferImg.pixels[displacement+1];
+            archiveImg.pixels[index+2] = bufferImg.pixels[displacement+2];
+            archiveImg.pixels[index+3] = 255;
+            //dispMap.pixels[mapIndex+3] = 0;
+          }
+          xCount++;
         }
+        yCount++;
+        xCount = 0;
       }
-  pupImg.updatePixels();
-  bufferImg.updatePixels();
-  image(bufferImg,0,0);
 
+
+  archiveImg.updatePixels();
+  bufferImg.updatePixels();
+  dispMap.updatePixels();
+  image(archiveImg,0,0);
+  //image(dispMap,mouseX-dispMap.width/2,mouseY-dispMap.height/2);
+  //noLoop();
 
 }
 
